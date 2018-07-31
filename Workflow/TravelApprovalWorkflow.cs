bef36ba822ae44ca0cf75e85a0cb5db36d2d9a89
InfantIvanDhas I <InfantIvanDhas.I@emishealth.com>
2018-07-31 16:25:10 +0530

    Initial Commit

---
+++ Workflow/TravelApprovalWorkflow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using tomware.Microwf.Core;
+
+namespace Emis.Internal.Travel.Workflow
+{
+	public class TravelApprovalWorkflow : WorkflowDefinitionBase
+	{
+		public const string TYPE = "TravelApprovalWorkflow";
+		public override string Type
+		{
+			get { return TYPE; }
+		}
+
+		public override List<Transition> Transitions => throw new NotImplementedException();
+	}
+}
